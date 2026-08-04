04957ae855f6430aafd4296e6c40a4a383cdfc90
cardsandgiftske-maker <cardsandgiftske@gmail.com>
2026-08-04 18:45:50 +0400

    Update import path for generateRsvpEmailHtml

--- api/send-rsvp-email.ts
+++ api/send-rsvp-email.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 import { Resend } from 'resend';
-import { generateRsvpEmailHtml } from '../src/lib/emailTemplate';
+import { generateRsvpEmailHtml } from '@/lib/emailTemplate';
 
 export async function sendRsvpEmailHandler(req: any, res: any) {
   try {
